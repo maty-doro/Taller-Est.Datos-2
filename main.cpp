3b7478d861f4b7da01d3b37943bbc6891aa70d2a
Gael Ortega <177061103+Gael-OC@users.noreply.github.com>
2025-10-19 15:46:12 -0300

    Limpiar main

--- main.cpp
+++ main.cpp
@@ -2,23 +2,6 @@
 #include "SparseMatrix.h"
 
 int main() {
-    std::cout << "Probando la implementacion de la Matriz" << std::endl;
-
-    SparseMatrix miMatriz;
-
-    miMatriz.printStoredValues();
-
-    miMatriz.add(10, 1, 2);
-    miMatriz.add(50, 3, 4);
-    miMatriz.add(25, 3, 2);
-    miMatriz.add(99, 8, 10);
-
-    miMatriz.printStoredValues();
-    
-    std::cout << "\nActualizando el valor en (1, 2) a 150..." << std::endl;
-    miMatriz.add(150, 1, 2);
-
-    miMatriz.printStoredValues();
-
+    std::cout << "Taller 2 Estructura de Datos" << std::endl;
     return 0;
 }
